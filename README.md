b6728f230a7c78dc0485139bfaebca2a9dab9658
Donquixot228 <kirilnoskov402@gmail.com>
2022-08-16 12:17:08 +0300

    ios run complete

--- README.md
+++
@@ -1,5 +0,0 @@
-# nft_appe
-
-## look
-https://user-images.githubusercontent.com/64096633/157738427-1e9c07ac-a97c-4da5-85e9-73b01e10b5c0.mp4
-
--- android/app/src/main/kotlin/com/example/nft_appe/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.nft_appe
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
