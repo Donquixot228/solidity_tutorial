09e74c84fe98cf4c28db34f53662b77b213051ef
Donquixot228 <kirilnoskov402@gmail.com>
2022-08-18 15:17:14 +0300

    prepare for new wallet_connect method

--- cryptoApp/lib/main.dart
+++ cryptoApp/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:solidity_tutorial/pages%20/home_page/home_page.dart';
@@ -5,23 +7,22 @@ import 'package:solidity_tutorial/services/notes_service.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => NotesService(),
-      child: const MyApp(),
-    ),
+    const MyApp(),
   );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'solidity tutorial',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: const HomePage(),
     );
   }
 }
--- cryptoApp/lib/pages /home_page/home_page.dart
+++ cryptoApp/lib/pages /home_page/home_page.dart
@@ -1,101 +1,17 @@
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../services/notes_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  final TextEditingController titleController = TextEditingController();
-  final TextEditingController descriptionController = TextEditingController();
-
-  @override
-  void dispose() {
-    super.dispose();
-    titleController.dispose();
-    descriptionController.dispose();
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var notesServices = context.watch<NotesService>();
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Notes'),
-      ),
-      body: notesServices.isLoading
-          ? const Center(
-        child: CircularProgressIndicator(),
-      )
-          : RefreshIndicator(
-        onRefresh: () async {},
-        child: ListView.builder(
-          itemCount: notesServices.notes.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(notesServices.notes[index].title),
-              subtitle: Text(notesServices.notes[index].description),
-              trailing: IconButton(
-                icon: const Icon(
-                  Icons.delete,
-                  color: Colors.red,
-                ),
-                onPressed: () {
-                  notesServices.deleteNote(notesServices.notes[index].id);
-                },
-              ),
-            );
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                title: const Text('New Note'),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextField(
-                      controller: titleController,
-                      decoration: const InputDecoration(
-                        hintText: 'Enter title',
-                      ),
-                    ),
-                    TextField(
-                      controller: descriptionController,
-                      decoration: const InputDecoration(
-                        hintText: 'Enter description',
-                      ),
-                    ),
-                  ],
-                ),
-                actions: [
-                  TextButton(
-                    onPressed: () {
-                      notesServices.addNote(
-                        titleController.text,
-                        descriptionController.text,
-                      );
-                      Navigator.pop(context);
-                    },
-                    child: const Text('Add'),
-                  ),
-                ],
-              );
-            },
-          );
-        },
+    return const Scaffold(
+      backgroundColor: Colors.white24,
+      body: Center(
+        child: Text('Initial view in binance api',style: TextStyle(color: Colors.white,fontSize: 30),),
       ),
     );
   }
